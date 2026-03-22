5b6645021cba34e7ed3490deac26c68f2ca9b154
Caius <caiuspb@proton.me>
2026-03-22 22:03:21 +0100

    initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# zoxide (ersetzt "z")
+zoxide init fish | source
+
+# optional: bessere History-Suche mit Pfeiltasten
+#bind \e\[A history-search-backward
+#bind \e\[B history-search-forward
+
+ alias l auf ls -ahl
+function l
+    ls -ahl $argv
+end
+
+starship init fish | source
